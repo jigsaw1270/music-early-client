737d00906e0e20068db7087f1e86fcbaf0e65f9e
jigsaw1270 <nomadshomoy@gmail.com>
2023-06-09 07:57:57 +0600

    do popular classes

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Classitems from '../components/Classitems';
 import useClasses from '../hooks/useClasses';
+import Cover from '../components/Cover';
+import image1 from '../assets/images/cover/cover2.jpg'
 
 const Classes = () => {
     const [classes] = useClasses();
     return (
-        <div className='grid grid-cols-3 gap-4'>
+       <div>
+       <Cover img={image1} title={'ALL OFFERED CLASSES'} subtitle={'Unlock your musical potential and embark on a journey of rhythm, melody, and harmony with our exceptional music classes. Whether youre a complete beginner or an experienced musician, our talented instructors are here to guide and inspire you.'}  ></Cover>
+         <div className='grid grid-cols-3 gap-4 py-4'>
             {
                 classes.map(item =><Classitems key={item._id} item={item}></Classitems>)
             }
         </div>
+       </div>
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Cover from '../components/Cover';
+import image1 from '../assets/images/cover/cover4.jpg'
+import image2 from '../assets/images/cover/cover1.jpg';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Cover  img={image1} title={'Popular classes'} subtitle={'Discover a wide range of musical instruments to choose from, including the timeless piano, versatile guitar, elegant violin, energetic drums, soulful saxophone, enchanting flute, and many more. Our classes cater to all ages and skill levels, ensuring that everyone can find their perfect fit.'} ></Cover>
+
+            <Cover img={image2} title={'Popular instructors'} subtitle={'Each class is led by a passionate and experienced instructor who will share their expertise and knowledge. They will not only teach you the technical aspects of playing an instrument but also help you develop your own unique style and musical expression.'}></Cover>
         </div>
     );
 };

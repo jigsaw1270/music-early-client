6ad4406dba0f6491566b9ff5b83475dac173356a
jigsaw1270 <nomadshomoy@gmail.com>
2023-06-10 23:05:09 +0600

    role created

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 import AuthProvider from './Provider/Authprovider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 
 
@@ -17,9 +20,11 @@ import AuthProvider from './Provider/Authprovider';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
  <AuthProvider>
- <div   className='max-w-screen-xl mx-auto'>
+<QueryClientProvider client={queryClient}>
+<div   className='max-w-screen-xl mx-auto'>
             <RouterProvider router={router} />
           </div>
+</QueryClientProvider>
 
  </AuthProvider>
   </React.StrictMode>,
--- src/pages/Alluser.jsx
+++ src/pages/Alluser.jsx
@@ -1,10 +1,96 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { FaTrashAlt, FaUserGraduate, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
+
 
 const Alluser = () => {
+    const { data: users =[],refetch} = useQuery(['users'], async()=>{
+        const res = await fetch('http://localhost:5000/users')
+        return res.json();
+    })
+
+
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleMakeInstructor = user =>{
+        fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an instructor Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+
+    }
+
+    const handleDelete = user => {
+
+    }
     return (
         <div>
-            <h2>alluser</h2>
+        <h3 className="my-4 text-3xl font-semibold">Total Users: {users.length}</h3>
+        <div className="overflow-x-auto">
+            <table className="table w-full table-zebra">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => <tr key={user._id}>
+                            <th>{index + 1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{ user.role === 'admin' ? 'admin' :
+                                <button onClick={() => handleMakeAdmin(user)} className="text-white bg-orange-600 btn btn-ghost"><FaUserShield></FaUserShield></button> 
+                                }</td>
+                            <td>{ user.role === 'instructor' ? 'instructor' :
+                                <button onClick={() => handleMakeInstructor(user)} className="text-white bg-green-600 btn btn-ghost"><FaUserGraduate></FaUserGraduate></button> 
+                                }</td>
+                            <td><button onClick={() => handleDelete(user)} className="text-white bg-red-600 btn btn-ghost"><FaTrashAlt></FaTrashAlt></button></td>
+                        </tr>)
+                    }
+                    
+                    
+                </tbody>
+            </table>
         </div>
+    </div>
     );
 };
 
